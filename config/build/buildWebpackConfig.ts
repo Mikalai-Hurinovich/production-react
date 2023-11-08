58d9dd1ee7bb1cbf8879b1e5e62fd045b6a8d2bc
Mikolai.Hurynovich <Mikolai.Hurynovich@ventionteams.com>
2023-11-08 16:22:12 +0300

    webpack optimization, postinstall added

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -17,12 +17,13 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             clean: true,
             publicPath: '/',
         },
+
         plugins: buildPlugins(options),
         module: {
             rules: buildLoaders(options),
         },
         resolve: buildResolvers(options),
-        devtool: isDev ? 'source-map' : undefined,
+        devtool: isDev ? 'eval-cheap-module-source-map' : undefined,
         devServer: isDev ? buildDevServer(options) : undefined,
     };
 }
